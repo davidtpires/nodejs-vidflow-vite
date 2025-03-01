4f35e6d50644abc8c7c55a81f5a763e4071bc1d8
David T. <davidt.info@gmail.com>
2025-03-01 10:12:53 -0300

    Criando uma prévia de produção

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
-    const busca = await axios.get("http://localhost:3000/videos");
+    const busca = await axios.get(
+      "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
+    );
 
     const videos = busca.data;
 
     videos.forEach((video) => {
       if (video.categoria == "") {
-
-
         throw new Error("Vídeo não tem categoria");
       }
       containerVideos.innerHTML += `
